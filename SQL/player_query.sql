4c8bd823cec3744f426244a47bf61c28161e5f97
caschkerke <5404529+caschkerke@users.noreply.github.com>
2020-04-25 10:16:58 -0500

    Resubmitting small fixes

---
+++ SQL/player_query.sql
@@ -0,0 +1,21 @@
+select current_players.appid, 
+       current_players.current_players, 
+       steam_data.appid, 
+       steam_data.release_date, 
+       steam_data.name, 
+       steam_data.postive_ratings, 
+       steam_data.negative_ratings, 
+       steam_data.average_playtime, 
+       steam_data.median_playtime, 
+       steam_data.owners, 
+       steam_data.price
+
+from current_players
+    inner join steam_data 
+        on current_players.appid = steam_data.appid;
+
+-- Joins tables
+-- SELECT current_players.appid, current_players.current_players, desired_database_here.desired_column_here, desired_database_here.desired_column_here
+-- FROM current_players
+-- JOIN "desired_database_here"
+-- ON current_players.appid = desired_database_here.desired_column_here;
